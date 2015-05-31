75a19daed58de7bee55f702d4fada871aafc6f79
PseudoKnight <michael.jt.smith@gmail.com>
2015-05-31 15:25:49 -0700

    Bump version to 1.4.1

--- src/main/java/me/pseudoknight/chspigot/CHSpigot.java
+++ src/main/java/me/pseudoknight/chspigot/CHSpigot.java
@@ -13,7 +13,7 @@ public class CHSpigot extends AbstractExtension {
     public static SpigotEventListener listener;
     
     public Version getVersion() {
-        return new SimpleVersion(1,4,0);
+        return new SimpleVersion(1,4,1);
     }
 
     @Override
